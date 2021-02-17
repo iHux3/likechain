5868d7597f10a9eac692dc1814802b24938cb483
iHux3 <35241224+iHux3@users.noreply.github.com>
2021-02-17 22:21:01 +0100

    ropsten added

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,32 @@
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const secrets = require("./secrets.json");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
-    }
-  } ,
-  compilers: {
-    solc: {
-      version: "^0.7.6",
-      settings: {
-        optimizer: {
-          enabled: false
+    contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        },
+        ropsten: {
+            provider: new HDWalletProvider(secrets.wallet, `https://ropsten.infura.io/v3/${secrets.infura}`),
+            network_id: 3
         }
-      }
+    } ,
+    compilers: {
+        solc: {
+        version: "^0.7.6",
+            settings: {
+                optimizer: {
+                enabled: false
+                }
+            }
+        }
+    },
+    mocha: {
+        reporter: 'eth-gas-reporter',
+        reporterOptions: {}
     }
-  },
-  mocha: {
-    reporter: 'eth-gas-reporter',
-    reporterOptions: {}
-  }
 };
